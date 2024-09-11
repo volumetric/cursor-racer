45d3ba15123f818cf300b64809771d5f35b5bae2
Vinit Agrawal <vinit@hellotars.com>
2024-09-11 10:51:23 +0530

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,11 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Press_Start_2P } from 'next/font/google'
 
-const pressStart2P = Press_Start_2P({ weight: '400', subsets: ['latin'] })
+const pressStart2P = Press_Start_2P({ 
+  weight: '400', 
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: "Cursor Race",
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
+  output: 'export',
+  trailingSlash: true,
 };
 
 export default nextConfig;
