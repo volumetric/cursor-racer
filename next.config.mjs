a00a499c953a74951022a539eea9aabd28e57020
Vinit Agrawal <vinit@hellotars.com>
2025-12-30 20:12:05 +0530

    Add webpack alias for Phaser.js in Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      phaser: 'phaser/dist/phaser.js',
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
